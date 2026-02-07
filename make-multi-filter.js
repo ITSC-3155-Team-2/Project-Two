2e233e8bc1a057a9afa816f536afb8119f38e4a6
Matthew Soukhaphon <mattsoukhaphon2@gmail.com>
2026-02-07 17:46:06 -0500

    Update make-multi-filter.js

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,23 +1,34 @@
 'make strict';
 
-var originalArray = [1, 2, 3]
+var originalArray = [1, 2, 3];
 
 function MakeMultiFilter(originalArray) {
+    var currentArray = originalArray;
+
+    for (var i = 0; i < currentArray.length; i++) {
+        arrayFilterer(filterCritera, callback);
+    }
+
+    function arrayFilterer(filterCritera, callback) {
+        if (filterCriteria) {
+            currentArray.splice()
+        } else {
+
+        }
+    }
     return arrayFilterer();
 }
 
-function arrayFilterer(array) {
-    var currentArray = originalArray;
-}
-
-function arrayFilterer(func) {
-    return 
-}
-
 function filterCritera(element) {
-    
+    if (element >= 1) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+/*
 function callback() {
 
 }
+*/
