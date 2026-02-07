2b20b80d4364fba17c581b4ddec43bf69edb5fca
Matthew Soukhaphon <mattsoukhaphon2@gmail.com>
2026-02-06 20:30:54 -0500

    add make-multi-filter.js

---
+++ make-multi-filter.js
@@ -0,0 +1,23 @@
+'make strict';
+
+var originalArray = [1, 2, 3]
+
+function MakeMultiFilter(originalArray) {
+    return arrayFilterer();
+}
+
+function arrayFilterer(array) {
+    var currentArray = originalArray;
+}
+
+function arrayFilterer(func) {
+    return 
+}
+
+function filterCritera(element) {
+    
+}
+
+function callback() {
+
+}
