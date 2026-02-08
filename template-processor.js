cf968c555d92d89db351f8181ac2e7fd6070c239
Joseph Denton <jdento15@charlotte.edu>
2026-02-08 14:26:08 -0500

    Update template-processor.js
    Filled out template-processor.js as V1

--- template-processor.js
+++ template-processor.js
@@ -1,1 +1,16 @@
+"use strict";
 
+function TemplateProcessor(template) {
+  this.template = template;
+}
+
+TemplateProcessor.prototype.fillIn = function (dictionary) {
+  return this.template.replace(/{{\s*(\w]+)\s*}}/g, function (match, property) {
+    if (Object.hasOwn(dictionary, property) {
+      return dictionary[property];
+    }
+
+    return "";
+  });
+}
+  
